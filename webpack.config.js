bda09ad263e1071c0e982a58b3629818d532cb16
Super <linzechao44@163.com>
2018-06-05 18:02:37 +0800

    [APPEND]webpack-dev-server 配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,43 @@
 const path = require('path')
 
 module.exports = {
+  // mode: env.production ? 'production' : 'development',
+  // devtool: env.production ? 'source-maps' : 'eval',
+  mode: 'development',
+  devtool: 'eval',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
+    // filename: '[chunkhash].js'
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader'
+        }]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src/components'),
+      '_': path.resolve(__dirname, 'src/router'),
+      '$': path.resolve(__dirname, 'src/utils')
+    }
   }
 }
